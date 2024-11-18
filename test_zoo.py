f06e12a3a6049f34077503ea287aa028b6545e67
mapsiir <113156776+prangrock@users.noreply.github.com>
2024-11-18 08:20:04 +0000

    Add test case and fix condition

--- test_zoo.py
+++ test_zoo.py
@@ -5,8 +5,23 @@ class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
 
+    def test_negativeage_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Don't have negative age")
+
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+
+    def test_elder_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
        
     # Add your additional test cases here.
 
--- zoo.py
+++ zoo.py
@@ -1,10 +1,12 @@
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if age < 0: #add cases for negative age
+            return "Don't have negative age"
+        elif 0 <= age <= 12: #from if 0 < age <= 12 to elif 0 <= age <= 12 because 0 must be in this condition
             return 50
-        elif 13 <= age < 20:
+        elif 13 <= age <= 20: #from elif 13 <= age < 20 to elif 13 <= age <= 20 because 20 must be in this condition
             return 100
-        elif 21 < age <= 60:
+        elif 21 <= age <= 60: #from elif 21 < age <= 60 to elif 21 <= age <= 60 because 13 must be in this condition
             return 150
-        elif age >= 60:
+        elif age > 60: #from  elif age >= 60 to elif age > 60 because 60 must be in previous condition
             return 100
